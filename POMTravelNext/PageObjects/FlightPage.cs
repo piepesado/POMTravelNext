1bd79d08e3e5bc077c74350005610bebc530490c
Diego Gastón <dienan16@gmail.com>
2017-11-10 19:58:24 -0300

    Writing Test Class

--- POMTravelNext/PageObjects/FlightPage.cs
+++ POMTravelNext/PageObjects/FlightPage.cs
@@ -24,12 +24,27 @@ namespace POMTravelNext.PageObjects
         private IWebElement returnDatePicker;
 
         //Leave and Return dropdowns
+        /*
         [FindsBy(How = How.Id, Using = "ucPWP_ctl14_12066_txtFlightArriveDate")]
         SelectElement oLeaveTime = new SelectElement(leaveTime);
         private IWebElement leaveTime;
-        
+        */
 
+        //Search button
+        [FindsBy(How = How.Id, Using = "ucPWP_ctl14_12066_btnSearch")]
+        private IWebElement searchButton;
 
+        public void SearchFlight(string fromField, string toField, string leaveDatePicker, string returnDatePicker)
+        {
+            this.fromField.SendKeys("Montevideo");
+            this.fromField.SendKeys(Keys.Tab);
+            this.toField.SendKeys("Arlington");
+            this.toField.SendKeys(Keys.Tab);
+            this.leaveDatePicker.SendKeys("03/03/2018");
+            this.returnDatePicker.SendKeys("03/20/2018");
+            searchButton.Click();
+
+        }
 
     }
 }
--- POMTravelNext/TestClass.cs
+++ POMTravelNext/TestClass.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using NUnit.Framework;
+using POMTravelNext.PageObjects;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace POMTravelNext
 {
     [TestFixture]
     public class TestClass
     {
+        IWebDriver driver;
         [SetUp]
-        public void TestMethod1()
+        public void SetUp()
         {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void BookFlight()
+        {
+            LoginPage log = new LoginPage(driver);
+            log.LogOn(userName, passWord, cidNumber);
         }
     }
 }
